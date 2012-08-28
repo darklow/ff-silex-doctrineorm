b1b4a138c1dbd662c30fcdb71994b297f96d9333
darklow <darklow@gmail.com>
2012-08-28 14:10:56 +0300

    Initial commit

---
+++ src/Entora/ServiceProvider/DoctrineORMServiceProvider.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Entora\ServiceProvider;
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Configuration as DBALConfiguration;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Configuration as ORMConfiguration;
+use Doctrine\ORM\Mapping\Driver\DriverChain;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\ORM\Mapping\Driver\XmlDriver;
+use Doctrine\ORM\Mapping\Driver\YamlDriver;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Cache\ApcCache;
+use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\Common\EventManager;
+
+use Silex\Application;
+use Silex\ExtensionInterface;
+
+class DoctrineORMServiceProvider implements \Silex\ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        $app = $this->setConfigDefaults($app);
+        foreach ($app['dbs.orm'] as $dbName => $dbConfig) {
+            $this->registerSingle($app, $dbName);
+        }
+
+        // Set default db entityManager using first db
+        $app['db.orm.em'] = $app->share(function ($app)
+        {
+            return $app['dbs.orm.em.'.array_keys($app['dbs.orm'])[0]];
+        });
+    }
+
+    private function setConfigDefaults($app)
+    {
+        $defaults = array(
+            /*'entities'              => array(
+                array('type' => 'annotation', 'path' => 'Entity', 'namespace' => 'Entity')
+            ),*/
+            'proxies_dir'           => 'cache/doctrine/Proxy',
+            'proxies_namespace'     => 'DoctrineProxy',
+            'auto_generate_proxies' => true,
+        );
+        foreach ($defaults as $key => $value) {
+            if (!isset($app['db.orm.'.$key])) {
+                $app['db.orm.'.$key] = $value;
+            }
+        }
+        return $app;
+    }
+
+    private function registerSingle(Application $app, $dbName)
+    {
+        $app['dbs.configuration.'.$dbName] = $app->share($this->configClosure($app, $dbName));
+        $app['dbs.orm.em.'.$dbName]        = $app->share(function ($app) use ($dbName)
+        {
+            $em = EntityManager::create($app['dbs'][$dbName], $app['dbs.configuration.'.$dbName]);
+
+            if (isset($app['db.mapping_types'])) {
+                foreach ($app['db.mapping_types'] as $mappingTypeName => $mappingTypeClass) {
+                    if (!Type::hasType($mappingTypeName)) {
+                        Type::addType($mappingTypeName, '\\'.$mappingTypeClass);
+                        $em->getConnection()->getDatabasePlatform()
+                            ->registerDoctrineTypeMapping($mappingTypeName, $mappingTypeName);
+                    }
+                }
+            }
+
+            return $em;
+        });
+    }
+
+
+    private function configClosure($app, $dbName)
+    {
+        return function() use($app, $dbName)
+        {
+            $config = new ORMConfiguration;
+            $cache  = ($app['debug'] == false) ? new ApcCache : new ArrayCache;
+            $config->setMetadataCacheImpl($cache);
+            $config->setQueryCacheImpl($cache);
+
+            $chain = new DriverChain;
+            foreach ((array)$app['dbs.orm'][$dbName]['entities'] as $entity) {
+                switch ($entity['type']) {
+                    case 'annotation':
+                        $driver = $config->newDefaultAnnotationDriver((array)$entity['path'], false);
+                        $chain->addDriver($driver, $entity['namespace']);
+                        break;
+                    /*case 'yml':
+                          $driver = new YamlDriver((array)$entity['path']);
+                          $driver->setFileExtension('.yml');
+                          $chain->addDriver($driver, $entity['namespace']);
+                          break;
+                      case 'xml':
+                          $driver = new XmlDriver((array)$entity['path'], $entity['namespace']);
+                          $driver->setFileExtension('.xml');
+                          $chain->addDriver($driver, $entity['namespace']);
+                          break;*/
+                    default:
+                        throw new \InvalidArgumentException(sprintf('"%s" is not a recognized driver', $type));
+                        break;
+                }
+            }
+
+            $config->setMetadataDriverImpl($chain);
+
+            $config->setProxyDir($app['db.orm.proxies_dir']);
+            $config->setProxyNamespace($app['db.orm.proxies_namespace']);
+            $config->setAutoGenerateProxyClasses($app['db.orm.auto_generate_proxies']);
+
+            return $config;
+        };
+    }
+
+    public function boot(Application $app)
+    {
+    }
+}
